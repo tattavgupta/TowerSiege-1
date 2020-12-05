9aa84656fd53956746bf0368c184d28f39d23f4b
tattavgupta <72901330+tattavgupta@users.noreply.github.com>
2020-12-05 13:14:15 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,22 @@
+class Box{
+    constructor(x,y,width,height){
+      var options={
+          restitution:0.5
+      }
+        this.body=Bodies.rectangle(x,y,width,height,options);
+        this.width=width;
+        this.height=height;
+        World.add(world,this.body);
+    }
+    display(){
+    var angle=this.body.angle
+    push ()
+    translate(this.body.position.x,this.body.position.y)
+    angleMode(RADIANS)
+    rotate (angle)
+    rectMode(CENTER)
+    fill(255,192,203)
+    rect(0,0,this.width,this.height)
+    pop()
+    }
+}
